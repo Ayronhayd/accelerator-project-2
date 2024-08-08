27cdc6bb2ccbaaa3260f6fe19fcad0661a8523d6
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-08-08 23:25:24 +0300

    Hero+ swiper +button

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,7 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
+import Swiper from 'swiper';
+import {Pagination} from 'swiper/modules';
+import 'swiper/css';
+// Navigation,
 // закрытие при нажатии на сылку
 const magicLink = document.querySelectorAll('.site-list__link');
 const checkbox = document.querySelector('.menu__burger-checkbox');
@@ -12,3 +11,20 @@ magicLink.forEach((btn) => {
     checkbox.click();
   });
 });
+
+
+const swiperHero = new Swiper('.hero__swiper', {
+  modules: [Pagination],
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+    type: 'bullets'
+  },
+});
+
+swiperHero.update();
