d061aff6de0d0d300751f073217097ade9a33e12
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-08-08 22:00:40 +0300

    header js закрытие при нажатии на сылку

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,13 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+// закрытие при нажатии на сылку
+const magicLink = document.querySelectorAll('.site-list__link');
+const checkbox = document.querySelector('.menu__burger-checkbox');
+
+magicLink.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    checkbox.click();
+  });
+});
